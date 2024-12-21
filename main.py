342f1cfe2a05ad2fce66e58849a26f0bfc16a7fb
ZHdanov Valery <becwal@yandex.ru>
2024-12-21 22:48:46 +0300

    Программа удаления всего из определенной папки

---
+++ main.py
@@ -0,0 +1,12 @@
+#Анализ дисков
+
+
+import os
+
+top = "/mnt/e/python/t3/ra/GGG/"
+
+for root, dirs, files in os.walk(top, topdown=False):
+    for name in files:
+        os.remove(os.path.join(root, name))
+    for name in dirs:
+        os.rmdir(os.path.join(root, name))
